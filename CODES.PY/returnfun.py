3c159491de269ff5113d0740c234bfa3106a4d23
Abhyuday-Goyal <107510592+Abhyuday-Goyal@users.noreply.github.com>
2022-08-06 01:22:12 +0530

    Update returnfun.py

--- CODES.PY/returnfun.py
+++ CODES.PY/returnfun.py
@@ -1,5 +1,7 @@
+#Return function in python is used to terminate the function and assign a value to the function which will be returned upon calling the function 
 @gfg_decorator
 def hello_decorator():
     print("gfg")
 
 hello_decorator=gfg_decorator(hello_decorator)
+
